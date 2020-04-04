341267738e163d3fbb4d128d62149f255a499bdc
Aleksey Karasev <karasev.dev@gmail.com>
2020-04-04 20:08:43 +0300

    fix intro section

--- frontend/src/components/Intro/Intro.js
+++ frontend/src/components/Intro/Intro.js
@@ -21,24 +21,24 @@ export default class Intro extends Component {
           <Title subClass={styles.intro__title}>
             <h1 className={`${styles.intro__name} ${styles.intro__name_main}`}>{label}</h1>
           </Title>
+          <div className={styles.intro__options}>
+            <Row>
+              <Col xs={6}>
+                <div className={styles.intro__choice}>
+                  <p className={styles.intro__text}>Проходи уровни, получай призы и прочий lorem</p>
+                  <Link className={`${styles.intro__link} ${styles.intro__link_play}`} to='./games'>Играть</Link>
+                </div>
+              </Col>
+              <Col xs={6}>
+                <div className={styles.intro__choice}>
+                  <p className={styles.intro__text}>Создай свой собственный уровень и нагни их всех!</p>
+                  <Link className={`${styles.intro__link} ${styles.intro__link_create}`} to='./creativity'>Создать</Link>
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className={styles.intro__compass}></div>
         </Container>
-        <div className={styles.intro__options}>
-          <Row>
-            <Col xs={6}>
-              <div className={styles.intro__choice}>
-                <p className={styles.intro__text}>Проходи уровни, получай призы и прочий lorem</p>
-                <Link className={`${styles.intro__link} ${styles.intro__link_play}`} to='./games'>Играть</Link>
-              </div>
-            </Col>
-            <Col xs={6}>
-              <div className={styles.intro__choice}>
-                <p className={styles.intro__text}>Создай свой собственный уровень и нагни их всех!</p>
-                <Link className={`${styles.intro__link} ${styles.intro__link_create}`} to='./creativity'>Создать</Link>
-              </div>
-            </Col>
-          </Row>
-        </div>
-        <span className={styles.intro__compass}></span>
       </div>
     }
 
