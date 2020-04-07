7c4b7273ca3fd8bc95c666457deb59ddc1dd4646
Aleksey Karasev <karasev.dev@gmail.com>
2020-04-07 20:06:17 +0300

    add responsive img/fix classname

--- frontend/src/components/Level/Level.js
+++ frontend/src/components/Level/Level.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Level.module.scss';
 import Button from '../UI/Button/Button';
+import img from './img/level-choice_1.jpg';
 
 const Level = ({number, description}) => {
 
   return (
-    <li className={styles.levels__item}>
-      <img className={styles.levels__image} alt='pic.Level'></img>
-      <div className={styles.levels__wrapper}>
-        <h3 className={styles.levels__title}>Вступление</h3>
-        <span className={styles.levels__number}>Уровень: {number}</span>
-        <p className={styles.levels__description}>{description}</p>
+    <li className={styles.item}>
+      <div className={styles.item__image}>
+        <img src={img} className={styles.image} alt='pic.Level'></img>
+      </div>
+      <div className={styles.wrapper}>
+        <h3 className={styles.title}>Вступление</h3>
+        <span className={styles.number}>Уровень: {number}</span>
+        <p className={styles.description}>{description}</p>
         <Button domType={'simple'}>Начать</Button>
       </div>
     </li>
